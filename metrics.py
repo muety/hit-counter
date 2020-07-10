f87ffb16ee2b431792e81f5d0cd7f64333d10b7a
Ferdinand Mütsch <mail@ferdinand-muetsch.de>
2020-07-10 11:59:40 +0200

    fix(metrics): periodically check for new data to register as labels

--- metrics.py
+++ metrics.py
@@ -1,9 +1,11 @@
 import time
-from threading import Lock
+from threading import Lock, Timer
 
 from prometheus_client import Gauge
 
 METRICS_PREFIX = 'hitcounter'
+CACHE_TIMEOUT_SEC = 5
+REGISTER_INTERVAL_SEC = 120
 
 cached_data = None
 last_fetched = 0
@@ -12,11 +14,16 @@ lock = Lock()
 
 def init_metrics(db_connection):
     g = Gauge(f'{METRICS_PREFIX}_hits_total', 'Total number of hits', ['site', 'path'])
+    register_labels(db_connection, g)
 
+    Timer(REGISTER_INTERVAL_SEC, lambda: register_labels(db_connection, g)).start()
+
+
+def register_labels(db_connection, gauge):
     url_counts = db_connection.getTopUrls(db_connection.get_connection(), -1)
     for url in url_counts['urls']:
         site, path = _split_url(url)
-        g.labels(site, path).set_function(_get_resolver(db_connection, site, path))
+        gauge.labels(site, path).set_function(_get_resolver(db_connection, site, path))
 
 
 def resolve_label_count(db, site, path):
@@ -24,7 +31,7 @@ def resolve_label_count(db, site, path):
 
     lock.acquire()
 
-    if time.monotonic() - last_fetched >= 5:
+    if time.monotonic() - last_fetched >= CACHE_TIMEOUT_SEC:
         cached_data = db.getTopUrls(db.get_connection(), -1)
         last_fetched = time.monotonic()
 
